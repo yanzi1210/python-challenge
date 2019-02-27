4a5d917b4243db0bf4be3a022a0c837d37b4b15a
Yan Xu <44480701+yanzi1210@users.noreply.github.com>
2019-02-26 18:57:33 -0600

    Add files via upload

---
+++ PyBank/main.py
@@ -0,0 +1,39 @@
+import os
+import csv
+budget_data = '/Users/macowner/Desktop/03-Python/Instructions/PyBank/Resources/budget_data.csv'
+total_months = 0
+total_amount = 0
+profit = []
+time = []
+profitChange = 0
+
+
+with open(budget_data, 'r') as csvfile:
+
+    # Split the data on commas
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    header = next(csvreader)
+    for row in csvreader:
+        total_months = total_months + 1
+        total_amount = total_amount + int(row[1])
+        profit.append(int(row[1]))
+        time.append(row[0])
+average = round(total_amount/total_months, 2)
+a = profit.index(max(profit))
+b = profit.index(min(profit))
+output = (
+     "Financial Analysis\n"
+     "----------------------------\n"
+     f"Total Months: {total_months}\n"
+     f"Total: ${total_amount}\n"
+     f"Average Change: ${average}\n"
+     f"Greatest Increase in Profit: {time[a]}  $({max(profit)})\n"
+     f"Greatest Decrease in Profit: {time[b]}  $({min(profit)})")
+
+# Print the output (to terminal)
+print(output)
+output_file = os.path.join('new_file.txt')
+# Export the results to text file
+with open(output_file, "w") as txt_file:
+    txt_file.write(output)
